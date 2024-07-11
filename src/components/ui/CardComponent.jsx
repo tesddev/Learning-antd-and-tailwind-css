9dc32f2ec44e37cf44ba0b673f55185c0763cb10
tesddev <tesleem.amuda@gmail.com>
2024-07-11 13:31:38 +0100

    customize card component to use passed styline

--- src/components/ui/CardComponent.jsx
+++ src/components/ui/CardComponent.jsx
@@ -1,16 +1,17 @@
 import { Card } from "antd";
-import { MehOutlined } from "@ant-design/icons";
-
+const col = "red-200"
+const theClassName = "h-[50px] w-[50px] bg-red-200 rounded-full flex justify-center items-center text-3xl"
 // eslint-disable-next-line react/prop-types
-const CardComponent = ({numbers, text}) => (
+const CardComponent = ({numbers, text, image, theClassName}) => (
+  
   <Card
     style={{
-      width: 300,
+      width: 430,
     }}
   >
     <div className="flex ">
-      <div className="h-[50px] w-[50px] bg-red-200 rounded-full flex justify-center items-center text-3xl">
-        <MehOutlined />
+      <div className={theClassName}>
+        {image}
       </div>
       <div className="px-6">
         <h3 className="text-3xl text bold">{numbers}</h3>
